005ae22ff98caf602b564a1ce6dbea38f22e8467
Mikhail Panyushkin <mpanius@gmail.com>
2025-05-09 19:14:59 +0300

    Добавлено использование класса Str из Illuminate для улучшения обработки строк в SpatieMediaLibraryFileUpload

--- src/Forms/Components/SpatieMediaLibraryFileUpload.php
+++ src/Forms/Components/SpatieMediaLibraryFileUpload.php
@@ -12,6 +12,7 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\MediaCollections\FileAdder;
 use Spatie\MediaLibrary\MediaCollections\MediaCollection;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Illuminate\Support\Str;
 use Throwable;
 
 class SpatieMediaLibraryFileUpload extends FileUpload
